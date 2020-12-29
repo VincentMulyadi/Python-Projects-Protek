11366830ff343a197525af357e05580e320ca4e6
VincentMulyadi <71622488+VincentMulyadi@users.noreply.github.com>
2020-12-29 14:40:43 +0700

    Update project04.py

--- Praktikum 10/project04.py
+++ Praktikum 10/project04.py
@@ -10,16 +10,22 @@ for data in list_data:
 
 cari = input('Masukkan NIM yang mau dicari : ')
 
-if cari in new_list[1]:
-    a = 1
-    for lists in new_list:
-        if cari == new_list[a][0]:
-            print('Data Mahasiswa')
-            print('NIM    : '+ new_list[a][0])
-            print('Nama   : '+ new_list[a][1])
-            print('Alamat : '+ new_list[a][2])
-            break
-        else:
-            a += 1
-else:
+new_hasil = False
+x = 0
+for lists in new_list:
+    if cari in new_list[x]:
+        a = 0
+        for lists in new_list:
+            if cari == new_list[a][0]:
+                print('Data Mahasiswa')
+                print('NIM    : '+ new_list[a][0])
+                print('Nama   : '+ new_list[a][1])
+                print('Alamat : '+ new_list[a][2])
+                new_hasil = True
+                break
+            else:
+                a += 1
+    x += 1
+
+if new_hasil == False :
     print('Data mahasiswa tidak ditemukan')
